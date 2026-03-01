ff0cf2e33b96f8027447458984a03eec8d35b359
Dario Kilian <dek@live.com.ar>
2026-03-01 20:08:41 -0300

    separado mi item detail e item detail container

---
+++ src/components/ItemDetail.jsx
@@ -0,0 +1,22 @@
+function ItemDetail ({ item }) {
+    return (  
+    <div className="card lg:card-side bg-base-100 shadow-sm">
+            <figure>
+                <img
+                    src={item.thumbnail}
+                    alt={item.title} />
+            </figure>
+            <div className="card-body">
+                <h2 className="card-title">{item.title}</h2>
+                <h3>${item.price} </h3>
+                <p>stock: {item.stock}</p>
+                <p>{item.description}</p>
+                <div className="card-actions justify-end">
+                <button className="btn btn-primary">Agregar al carrito</button>
+                </div>
+            </div>
+        </div>
+        )
+}
+
+export default ItemDetail
--- src/components/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react"
 import { useParams } from "react-router";
+import ItemDetail from "./ItemDetail";
 
 function ItemDetailContainer () {
     const [item, setItem] = useState ()
@@ -20,20 +21,7 @@ function ItemDetailContainer () {
     }
 
     return (
-        <div className="card lg:card-side bg-base-100 shadow-sm">
-            <figure>
-                <img
-                    src={item.thumbnail}
-                    alt={item.title} />
-            </figure>
-            <div className="card-body">
-                <h2 className="card-title">{item.title}</h2>
-                <p>{item.description}</p>
-                <div className="card-actions justify-end">
-                <button className="btn btn-primary">Agregar al carrito</button>
-                </div>
-            </div>
-        </div>
+        <ItemDetail item={item} />
     )
 }
 
